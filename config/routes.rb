4234ab99b1502e4f36040ba1dcf0cc042efde550
matej poje <matej7poje@gmail.com>
2022-12-15 08:27:03 +0100

    Add comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:create, :destroy]
+  resources :comments, only: [:update, :destroy, :create]
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
